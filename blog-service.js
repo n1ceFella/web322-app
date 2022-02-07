72a5093b4a80d46c43a08d35be3e48ac4bd83d2b
Vladimir Labliuk <50242030+n1ceFella@users.noreply.github.com>
2022-02-06 19:30:54 -0500

    modified version

---
+++ blog-service.js
@@ -0,0 +1,45 @@
+const fs = require("fs"); 
+
+var posts = [];
+var categories = [];
+
+module.exports.initialize = function() {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./data/posts.json', 'utf8', (err, data) => {
+            if (err) throw err;
+            else {
+                posts = JSON.parse(data,posts);
+                fs.readFile('./data/categories.json', 'utf8', (err, data) => {  
+                    if (err) throw err;
+                    else {
+                        categories = JSON.parse(data,categories);
+                        console.log("data retrieved");
+                        resolve("File has been read");
+                    }
+                })
+            }                                                                                     
+        })
+    });
+}
+
+module.exports.getAllPosts = function() {
+    return new Promise((resolve, reject) => {
+        posts.length == 0 ? reject('No results returned') : resolve(posts)
+    })
+};
+module.exports.getPublishedPosts = function() {
+    var temp = [];
+    for (i = 0; i < posts.length; i++) {
+        if (posts[i].published) {
+            temp.push(posts[i]);
+        }
+    }
+    return new Promise((resolve, reject) => {
+        temp.length == 0 ? reject('No results returned') : resolve(temp); 
+    })
+};
+module.exports.getCategories = function() {
+    return new Promise((resolve, reject) => {
+        categories.length == 0 ? reject('No results returned') : resolve(categories) ; 
+    })
+}
--- server.js
+++ server.js
@@ -1,5 +1,57 @@
-var x = function(){
-    for(i = 0; i < 10; i++){
-        console.log(i);
-    }
+const express = require("express");
+// const { listen } = require("express/lib/application");
+// const { send } = require("express/lib/response");
+const _path = require("path");
+const _blogService = require("./blog-service");
+const _server = express();
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+_server.use(express.static('public')); 
+
+function onHttpStart() {
+    console.log("Express http server listening on port: " + HTTP_PORT);
 }
+ 
+_server.get("/", (req, res) => {
+    res.redirect('/about');
+});
+
+_server.get("/about", (req, res) => {
+    res.sendFile(_path.join(__dirname, './views/about.html'));
+});
+
+_server.get("/blog", (req, res) => {
+    _blogService.getPublishedPosts().then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        return {"Error message": err.message};
+    })
+})
+
+_server.get("/posts", (req, res) => {
+    _blogService.getAllPosts().then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        return {"Error message": err.message};
+    })
+})
+
+_server.get("/categories", (req, res) => {
+    _blogService.getCategories().then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        return {"Error message": err.message};
+    })
+})
+
+_server.get("*", (req, res) => {
+    res.sendFile(_path.join(__dirname, "./views/error.html"));
+})
+
+_blogService.initialize().then(() => {
+    _server.listen(HTTP_PORT, onHttpStart);
+}).catch((err) => {
+    console.log(err);
+    res.status(404).send("File couldn't be read");
+})
