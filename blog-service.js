81c9f816e1344c2a701239d51bdf7a5a5dc6680f
Vladimir Labliuk <50242030+n1ceFella@users.noreply.github.com>
2022-03-27 17:33:36 -0400

    blog-service.js fuction set up for db use

--- blog-service.js
+++ blog-service.js
@@ -25,25 +25,40 @@ var Category = sequelize.define('Category', {
     category: Sequelize.STRING
 });
 
-
+Post.belongsTo(Category, {foreignKey: 'category'});
 
 module.exports.initialize = function() {
     return new Promise((resolve, reject) => {
-        reject();
+        sequelize.sync().then(function(){resolve()}).catch(function(){reject("unable to sync the database")});
 });
 
 }
 
 module.exports.getAllPosts = function() {
     return new Promise((resolve, reject) => {
-        reject();
-});
+        sequelize.sync().then(function () {
+
+            Posts.findAll().then(function(data){
+                resolve(data);
+            }).catch(function(){
+                reject("no results returned");
+            });
+        });
+    });
 
 };
 module.exports.getPublishedPosts = function() {
-    return new Promise((resolve, reject) => {
-        reject();
-});
+    sequelize.sync().then(function () {
+        Posts.findAll({
+            where: {
+                published: true //might be ID
+            }
+        }).then(function(data){
+            resolve(data);
+        }).catch(function(){
+            reject("no results returned");
+        });
+    });
 
 };
 module.exports.getCategories = function() {
@@ -55,34 +70,98 @@ module.exports.getCategories = function() {
 
 module.exports.getPostsByCategory = (category) => {
     return new Promise((resolve, reject) => {
-        reject();
+        sequelize.sync().then(function (categoryID) {
+            Posts.findAll({
+                where: {
+                    category: categoryID //might be ID
+                }
+            }).then(function(data){
+                resolve(data);
+            }).catch(function(){
+                reject("no results returned");
+            });
+        });
 });
-
 }
 
-module.exports.getPublishedPostsByCategory = (category) => {
-    return new Promise((resolve, reject) => {
-        reject();
-});
+module.exports.getPublishedPostsByCategory = (categoryID) => {
+    sequelize.sync().then(function () {
+        Posts.findAll({
+            where: {
+                published: true,
+                category : categoryID
+            }
+        }).then(function(data){
+            resolve(data);
+        }).catch(function(){
+            reject("no results returned");
+        });
+    });
 
 }
 
 module.exports.getPostsByMinDate = (minDateStr) => {
-
     return new Promise((resolve, reject) => {
-        reject();
-});
-
+        sequelize.sync().then(function (categoryID) {
+            const { gte } = Sequelize.Op;
+            Posts.findAll({
+                where: {
+                    postDate:{[gte]: new Date(minDateStr)}
+                }
+            }).then(function(data){
+                resolve(data);
+            }).catch(function(){
+                reject("no results returned");
+            });
+        });
+    });
 }
 
 module.exports.getPostById = (id) => {
     return new Promise((resolve, reject) => {
-        reject();
-});
+        sequelize.sync().then(function (postID) {
+            Posts.findAll({
+                where: {
+                    id: postID //might be ID
+                }
+            }).then(function(data){
+                resolve(data);
+            }).catch(function(){
+                reject("no results returned");
+            });
+        });
+    });
+}
+
+module.exports.getCategories = () => {
+    return new Promise((resolve, reject) => {
+        sequelize.sync().then(function () {
+            Category.findAll().then(function(data){
+                resolve(data);
+            }).catch(function(){
+                reject("no results returned");
+            });
+        });
+    });
 }
 
 module.exports.addPost = (postData) => {
-    return new Promise((resolve, reject) => {
-        reject();
-});
+    postData.published = (postData.published) ? true : false;
+    for (const prop in obj) {
+        if(prop == ""){
+            prop = null;
+        }
+    }
+    postData.postDate = new Date();
+    Post.create({
+        body: postData.body,
+        title: postData.title,
+        postDate: postData.postDate,
+        featureImage: postData.featureImage,
+        published: postData.published
+    }).then(function (post) {
+        resolve(post)
+    }).catch(function(){
+        reject("unable to create post");
+    });
 }
